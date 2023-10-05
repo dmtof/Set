ea57ab1c6c794a6c2071a7d4a0330c7ef76d3431
dmtof <dmtoof@gmail.com>
2023-10-05 19:38:30 +0300

    commit

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,26 +1,55 @@
-import ErrorRepository from '../basic';
+import Team from '../basic';
 
-test('Объект должен находить код ошибки', () => {
-  const code = new ErrorRepository();
-  expect(code.translate(1)).toBe('Файл не найден');
+// add - - - - - - - - - - - - - - - - - - - - - - - -
+
+test('Объект должен создаваться', () => {
+  const unit = new Team().add('Bowman');
+  expect(unit.size).toBe(1);
 });
 
-test('Объект должен находить код ошибки', () => {
-  const code = new ErrorRepository();
-  expect(code.translate(2)).toBe('Сетевая ошибка');
+test('Объект должен создаваться', () => {
+  const units = new Team();
+  const unit1 = units.add('Daemon');
+  const unit2 = unit1.add('Bowman');
+  expect(unit2.size).toBe(2);
 });
 
-test('Объект должен находить код ошибки', () => {
-  const code = new ErrorRepository();
-  expect(code.translate(3)).toBe('Файл поврежден');
+test('Объект не должен создаваться', () => {
+  expect(() => new Team().add('Boy')).toThrow();
 });
 
-test('Объект не должен находить код ошибки', () => {
-  const code = new ErrorRepository();
-  expect(code.translate(4)).toBe('Unknown error');
+test('Объект не должен создаваться', () => {
+  const unit = new Team();
+  unit.add('Bowman');
+  unit.add('Daemon');
+  expect(() => unit.add('Daemon')).toThrow();
 });
 
-test('Объект не должен находить код ошибки', () => {
-  const code = new ErrorRepository();
-  expect(code.translate('asdds')).toBe('Unknown error');
+// addAll - - - - - - - - - - - - - - - - - - - - - - - -
+
+test('Объект должен создаваться', () => {
+  const unit = new Team().addAll('Bowman', 'Zombie', 'Daemon');
+  expect(unit.size).toBe(3);
+});
+
+test('Объект должен создаваться', () => {
+  const unit = new Team().addAll('Zombie', 'Bowman', 'Zombie', 'Daemon');
+  expect(unit.size).toBe(3);
+});
+
+test('Объект должен создаваться', () => {
+  const unit = new Team().addAll('Zom', 'Bow', 'Zom', 'Daemon');
+  expect(unit.size).toBe(1);
+});
+
+// toArray - - - - - - - - - - - - - - - - - - - - - - - -
+
+test('Объект должен создаваться', () => {
+  const unit = new Team();
+  unit.addAll('Zombie', 'Bowman', 'Zombie', 'Daemon');
+  expect(unit.toArray()).toEqual([
+    { name: 'Zombie', type: 'Zombie' },
+    { name: 'Bowman', type: 'Bowman' },
+    { name: 'Daemon', type: 'Daemon' },
+  ]);
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,13 +1,43 @@
-export default class ErrorRepository {
+export default class Team {
   constructor() {
-    this.dataError = new Map([[1, 'Файл не найден'], [2, 'Сетевая ошибка'], [3, 'Файл поврежден']]);
+    this.members = new Set();
+    this.bowman = { name: 'Bowman', type: 'Bowman' };
+    this.swordsman = { name: 'Swordsman', type: 'Swordsman' };
+    this.magician = { name: 'Magician', type: 'Magician' };
+    this.daemon = { name: 'Daemon', type: 'Daemon' };
+    this.undead = { name: 'Undead', type: 'Undead' };
+    this.zombie = { name: 'Zombie', type: 'Zombie' };
+    this.team = [this.bowman, this.swordsman, this.magician, this.daemon, this.undead, this.zombie];
   }
 
-  translate(code) {
-    if (this.dataError.has(code) && typeof code === 'number') {
-      const result = this.dataError.get(code);
-      return result;
+  add(unit) {
+    if (typeof unit === 'string' && this.team.find((a) => a.name === unit)) {
+      const indexUnit = this.team.findIndex((a) => a.name === unit);
+      if (!this.members.has(this.team[indexUnit])) {
+        this.members.add(this.team[indexUnit]);
+        return this.members;
+      }
+      throw new Error('Персонаж уже добавлен в команду!');
+    } else {
+      throw new Error('Персонаж не найден!');
     }
-    return 'Unknown error';
+  }
+
+  addAll(...units) {
+    units.forEach((arg) => {
+      if (typeof arg === 'string' && this.team.find((a) => a.name === arg)) {
+        const indexUnit = this.team.findIndex((a) => a.name === arg);
+        this.members.add(this.team[indexUnit]);
+      }
+    });
+    return this.members;
+  }
+
+  toArray() {
+    const result = [];
+    for (const unit of this.members) {
+      result.push(unit);
+    }
+    return result;
   }
 }
